3db74d1db0e06e7f35206f843c7c24478b945c03
Renfred Harper <Renfred93@gmail.com>
2013-07-10 09:34:40 -0400

    Updated player sprite sheet and added basic walking animation

--- pylletTown.py
+++ pylletTown.py
@@ -5,18 +5,77 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self, location, *groups):
 		super(Player, self).__init__(*groups)
 		self.image = pygame.image.load('tiles/player.png')
+		self.imageDefault = self.image.copy()
 		self.rect = pygame.Rect(location, (64,64))
+		self.orient = 'down' 
+		self.holdTime = 0
+		self.walking = False
+		self.dx = 0
+		
+	def setSprite(self):
+		# Resets the player sprite sheet to its default position 
+		# and scrolls it to the necessary position for the current orientation
+		self.image = self.imageDefault.copy()
+		if self.orient == 'up':
+			self.image.scroll(0, -64)
+		elif self.orient == 'down':
+			self.image.scroll(0, 0)
+		elif self.orient == 'left':
+			self.image.scroll(0, -128)
+		elif self.orient == 'right':
+			self.image.scroll(0, -192)
 		
 	def update(self, dt, game):
 		key = pygame.key.get_pressed()
-		if key[pygame.K_LEFT]:
-			self.rect.x -= 64
-		if key[pygame.K_RIGHT]:
-			self.rect.x += 64
+		# Setting orientation and sprite based on key input: 
 		if key[pygame.K_UP]:
-			self.rect.y -= 64
-		if key[pygame.K_DOWN]:
-			self.rect.y += 64
+			if not self.walking:
+				if self.orient != 'up':
+					self.orient = 'up'
+					self.setSprite()
+				self.holdTime += dt
+		elif key[pygame.K_DOWN]:
+			if not self.walking:
+				if self.orient != 'down':
+					self.orient = 'down'
+					self.setSprite()	
+				self.holdTime += dt
+		elif key[pygame.K_LEFT]:
+			if not self.walking:
+				if self.orient != 'left':
+					self.orient = 'left'
+					self.setSprite()
+				self.holdTime += dt
+		elif key[pygame.K_RIGHT]:
+			if not self.walking:
+				if self.orient != 'right':
+					self.orient = 'right'
+					self.setSprite()
+				self.holdTime += dt
+		else:
+			self.holdTime = 0
+		# Walking mode enabled if a button is held for 0.1 seconds
+		if self.holdTime >= 100:
+			self.walking = True
+		# Walking at 8 pixels per frame in the direction the player is facing 
+		if self.walking and self.dx < 64:
+			if self.orient == 'up':
+				self.rect.y -= 8
+				self.dx += 8
+			elif self.orient == 'down':
+				self.rect.y += 8
+				self.dx += 8
+			elif self.orient == 'left':
+				self.rect.x -= 8
+				self.dx += 8
+			elif self.orient == 'right':
+				self.rect.x += 8
+				self.dx += 8
+			self.image.scroll(-64, 0)
+		if self.dx == 64:
+			self.walking = False
+			self.setSprite()	
+			self.dx = 0
 		
 		game.tilemap.set_focus(self.rect.x, self.rect.y)
 		
@@ -28,8 +87,8 @@ class Game(object):
 		self.sprites = tmx.SpriteLayer()
 		start_cell = self.tilemap.layers['triggers'].find('playerStart')[0]
 		self.player = Player((start_cell.px, start_cell.py), self.sprites)
-		self.tilemap.layers.append(self.sprites)
-		
+		self.tilemap.layers.append(self.sprites)		
+			
 		while 1:
 			dt = clock.tick(30)
 
@@ -39,7 +98,7 @@ class Game(object):
 				if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 					return
 
-			self.tilemap.update(dt / 1000, self)
+			self.tilemap.update(dt, self)
 			screen.fill((255,255,255))
 			self.tilemap.draw(screen)
 			pygame.display.flip()
