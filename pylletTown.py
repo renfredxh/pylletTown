78161fd24b35772243ae7078e09487cb17043df4
Renfred Harper <Renfred93@gmail.com>
2013-07-14 00:26:24 -0400

    Implement SpriteLoop class

--- pylletTown.py
+++ pylletTown.py
@@ -95,16 +95,60 @@ class Player(pygame.sprite.Sprite):
 			self.dx = 0
 		
 		game.tilemap.set_focus(self.rect.x, self.rect.y)
+
+class SpriteLoop(pygame.sprite.Sprite):
+	"""A simple looped animated sprite.
+	
+	SpriteLoops require certain properties to be defined in the relevant
+	tmx tile:
+	
+	src - the source of the image that contains the sprites
+	width, height - the width and height of each section of the sprite that
+		will be displayed on-screen during animation
+	mspf - milliseconds per frame, or how many milliseconds must pass to 
+		advance onto the next frame in the sprite's animation 
+	frames - the number individual frames that compose the animation
+	"""
+	def __init__(self, location, cell, *groups):
+		super(SpriteLoop, self).__init__(*groups)
+		self.image = pygame.image.load(cell['src'])
+		self.defaultImage = self.image.copy()
+		self.width = int(cell['width'])
+		self.height = int(cell['height'])
+		self.rect = pygame.Rect(location, (self.width,self.height))
+		self.frames = int(cell['frames'])
+		self.frameCount = 0
+		self.mspf = int(cell['mspf']) # milliseconds per frame
+		self.timeCount = 0
+
+		
+	def update(self, dt, game):
+		self.timeCount += dt
+		if self.timeCount > self.mspf:
+			# Advance animation to the appropriate frame
+			self.image = self.defaultImage.copy()
+			self.image.scroll(-1*self.width*self.frameCount, 0)
+			self.timeCount = 0
+			
+			self.frameCount += 1
+			if self.frameCount == self.frames:
+				self.frameCount = 0
 		
 class Game(object):
 	def main(self, screen):
 		clock = pygame.time.Clock()
-
 		self.tilemap = tmx.load('palletTown.tmx', screen.get_size())
-		self.sprites = tmx.SpriteLayer()
+		self.players = tmx.SpriteLayer()
+		self.objects = tmx.SpriteLayer()
+		# Initializing other animated sprites
+		for cell in self.tilemap.layers['sprites'].find('src'):
+			SpriteLoop((cell.px,cell.py), cell, self.objects)
+		# Initializing player sprite
 		start_cell = self.tilemap.layers['triggers'].find('playerStart')[0]
-		self.player = Player((start_cell.px, start_cell.py), self.sprites)
-		self.tilemap.layers.append(self.sprites)		
+		self.player = Player((start_cell.px, start_cell.py), self.players)	
+
+		self.tilemap.layers.append(self.objects)
+		self.tilemap.layers.append(self.players)	
 			
 		while 1:
 			dt = clock.tick(30)
